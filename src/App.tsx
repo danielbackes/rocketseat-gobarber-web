745c3419fa1b7ef433fa9c77cf991b0321cbfc8c
Daniel Backes <danielbackes@protonmail.com>
2020-09-02 20:40:04 -0400

    Create a toast message component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+import { AuthProvider } from './hooks/AuthContext';
+
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
-import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
