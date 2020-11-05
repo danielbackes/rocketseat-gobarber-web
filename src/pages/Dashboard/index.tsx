537aca1b81daf05c1d2fbf44d41c56a8c1a0b78d
Daniel Backes <danielbackes@protonmail.com>
2020-11-05 17:25:20 -0400

    feat(react component): :sparkles: Create Dashboard's Next Appointment component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -27,6 +36,30 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Hours booked</h1>
+          <p>
+            <span>Today</span>
+            <span>Day 06</span>
+            <span>Monday</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Next Appointment</strong>
+            <div>
+              <img src={user.avatar_url} alt={user.name} />
+              <strong>{user.name}</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
