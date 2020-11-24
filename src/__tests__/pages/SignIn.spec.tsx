acae77cf038190166709cef894769985c01606f5
Daniel Backes <danielbackes@protonmail.com>
2020-11-24 19:11:31 -0400

    test(jest): :white_check_mark: Write the tests to the SignUp page

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -34,7 +34,8 @@ jest.mock('../../hooks/toast', () => {
 
 describe('SignIn Page', () => {
   beforeEach(() => {
-    mockedHistoryPush.mockClear();
+    mockedHistoryPush.mockReset();
+    mockedAddToast.mockReset();
   });
 
   it('should be able to sign in', async () => {
@@ -49,27 +50,45 @@ describe('SignIn Page', () => {
 
     fireEvent.click(buttonElement);
 
-    await waitFor(() =>
-      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard'),
-    );
+    await waitFor(() => {
+      expect(mockedAddToast).not.toHaveBeenCalled();
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    });
+  });
+
+  it('should not be able to sign in without credentials', async () => {
+    const { getByText } = render(<SignIn />);
+
+    const buttonElement = getByText('Sign In');
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(getByText('E-mail required')).toBeTruthy();
+      expect(getByText('Password required')).toBeTruthy();
+      expect(mockedAddToast).not.toHaveBeenCalled();
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+    });
   });
 
   it('should not be able to sign in with invalid credentials', async () => {
     const { getByPlaceholderText, getByText } = render(<SignIn />);
 
     const emailField = getByPlaceholderText('E-mail');
-    const passwordField = getByPlaceholderText('Password');
     const buttonElement = getByText('Sign In');
 
-    fireEvent.change(emailField, { target: { value: 'not-valid-email' } });
-    fireEvent.change(passwordField, { target: { value: '123456' } });
+    fireEvent.change(emailField, { target: { value: 'invalid-email' } });
 
     fireEvent.click(buttonElement);
 
-    await waitFor(() => expect(mockedHistoryPush).not.toHaveBeenCalled());
+    await waitFor(() => {
+      expect(getByText('Invalid e-mail')).toBeTruthy();
+      expect(mockedAddToast).not.toHaveBeenCalled();
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+    });
   });
 
-  it('should display an error if login fails', async () => {
+  it('should display an error if sign in fails', async () => {
     mockedSignIn.mockImplementation(() => {
       throw new Error();
     });
@@ -85,12 +104,13 @@ describe('SignIn Page', () => {
 
     fireEvent.click(buttonElement);
 
-    await waitFor(() =>
+    await waitFor(() => {
       expect(mockedAddToast).toHaveBeenCalledWith(
         expect.objectContaining({
           type: 'error',
         }),
-      ),
-    );
+      );
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+    });
   });
 });
